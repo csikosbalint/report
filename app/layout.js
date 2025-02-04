b36f2eebe90d06478fb57b6fde3546da84db4be1
Balint Csikos <balint.csikos.priv@gmail.com>
2025-02-04 17:26:14 +0100

    Add AdSense code.

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
-import './globals.css'
-import { CookieConsent } from '@/components/cookie-consent'
+import "./globals.css";
+import { CookieConsent } from "@/components/cookie-consent";
 
-export default function RootLayout ({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
         <div>{children}</div>
         <CookieConsent />
       </body>
     </html>
-  )
+  );
 }
--- components/wrapper-ad.js
+++ components/wrapper-ad.js
@@ -1,11 +1,37 @@
-export function AdWrapper ({ position }) {
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function AdWrapper({ position }) {
+  const [ad, setAd] = useState(<>Ad Space ({position})</>);
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+      setAd(
+        <>
+          <ins
+            className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-8712767354684493"
+            data-ad-slot="1702336191"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+        </>
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }, [setAd]);
+
+  switch (position) {
+    case "right":
+  }
   return (
-    <div
-      className="w-[var(--ad-width)] bg-gray-100 p-4"
-    >
-      <div className='h-[600px] bg-gray-200 flex items-center justify-center text-gray-400'>
-        Ad Space ({position})
+    <div className="w-[var(--ad-width)]">
+      <div className="h-[600px] border">
+        {ad}
       </div>
     </div>
-  )
+  );
 }
