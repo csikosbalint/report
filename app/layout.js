108f4df51574205baa5e126c362780f51eaf90cc
Balint Csikos <balint.csikos.priv@gmail.com>
2025-02-04 17:26:14 +0100

    Add AdSense code.

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,18 @@
-import './globals.css'
-import { CookieConsent } from '@/components/cookie-consent'
+import "./globals.css";
+import { CookieConsent } from "@/components/cookie-consent";
 
-export default function RootLayout ({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-adsense-account" content="ca-pub-8712767354684493" />
       </head>
       <body>
         <div>{children}</div>
         <CookieConsent />
       </body>
     </html>
-  )
+  );
 }
