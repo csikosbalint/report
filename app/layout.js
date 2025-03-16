cd8b653e93c6fdd9e9b967adeb908b411705b7d1
Balint Csikos <balint.csikos.priv@gmail.com>
2025-03-16 11:55:15 +0100

    Quickfix for ads code.

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8712767354684493"
-          crossOrigin="anonymous"
+          crossorigin="anonymous"
         ></script>
       </head>
       <body>
